dc33535d57b7d7bcf576e50fbd5c3627c9de60f2
fr13think <fr13thinkpad@outlook.com>
2024-07-19 21:00:15 +0000

    update main

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ st.set_page_config(
 st.sidebar.image("Logoo.png")
 
 # Tambahkan instruksi tentang cara menggunakan aplikasi ke bilah sisi
-st.sidebar.header("Cara Penggunaan Risk Analyzer GPT")
+st.sidebar.header("Cara Penggunaan Risk Analyzer GPT v.1.1.10")
 
 with st.sidebar:
     # Tambahkan bidang input pemilihan model ke bilah samping
